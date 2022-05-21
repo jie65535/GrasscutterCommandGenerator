06b9055dfb8aeaa2954ba467288a7d66726d2981
jie65535 <jie65535@qq.com>
2022-05-21 15:09:18 +0800

    i18n Update

--- Source/GrasscutterTools/Forms/FormGachaBannerEditor2.Designer.cs
+++ Source/GrasscutterTools/Forms/FormGachaBannerEditor2.Designer.cs
@@ -61,11 +61,11 @@ namespace GrasscutterTools.Forms
             this.LblCostItem = new System.Windows.Forms.Label();
             this.LblPrefabPath = new System.Windows.Forms.Label();
             this.RbCostItem223 = new System.Windows.Forms.RadioButton();
-            this.GrpPurplePool = new System.Windows.Forms.GroupBox();
+            this.GrpFallbackPool = new System.Windows.Forms.GroupBox();
             this.ListFallbackItems = new System.Windows.Forms.ListView();
             this.ColFallbackId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ColFallbackName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.GrpYellowPool = new System.Windows.Forms.GroupBox();
+            this.GrpUpPool = new System.Windows.Forms.GroupBox();
             this.ListUpItems = new System.Windows.Forms.ListView();
             this.ColUpId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ColUpName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -73,13 +73,13 @@ namespace GrasscutterTools.Forms
             this.BtnGen = new System.Windows.Forms.Button();
             this.TxtJson = new System.Windows.Forms.TextBox();
             this.BtnParse = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.GrpWeights = new System.Windows.Forms.GroupBox();
             this.LnkWeightHelp = new System.Windows.Forms.LinkLabel();
             this.TxtWeight4 = new System.Windows.Forms.TextBox();
             this.TxtWeight5 = new System.Windows.Forms.TextBox();
             this.LblWeight4 = new System.Windows.Forms.Label();
             this.LblWeight5 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.GrpBalance = new System.Windows.Forms.GroupBox();
             this.TxtPoolWeight4 = new System.Windows.Forms.TextBox();
             this.TxtPoolWeight5 = new System.Windows.Forms.TextBox();
             this.LblPoolWeight4 = new System.Windows.Forms.Label();
@@ -90,11 +90,11 @@ namespace GrasscutterTools.Forms
             ((System.ComponentModel.ISupportInitialize)(this.NUDGachaType)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.NUDSortId)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.NUDScheduleId)).BeginInit();
-            this.GrpPurplePool.SuspendLayout();
-            this.GrpYellowPool.SuspendLayout();
+            this.GrpFallbackPool.SuspendLayout();
+            this.GrpUpPool.SuspendLayout();
             this.GrpJson.SuspendLayout();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.GrpWeights.SuspendLayout();
+            this.GrpBalance.SuspendLayout();
             this.SuspendLayout();
             // 
             // GrpBannerValues
@@ -349,12 +349,12 @@ namespace GrasscutterTools.Forms
             this.RbCostItem223.TabStop = true;
             this.RbCostItem223.UseVisualStyleBackColor = true;
             // 
-            // GrpPurplePool
+            // GrpFallbackPool
             // 
-            resources.ApplyResources(this.GrpPurplePool, "GrpPurplePool");
-            this.GrpPurplePool.Controls.Add(this.ListFallbackItems);
-            this.GrpPurplePool.Name = "GrpPurplePool";
-            this.GrpPurplePool.TabStop = false;
+            resources.ApplyResources(this.GrpFallbackPool, "GrpFallbackPool");
+            this.GrpFallbackPool.Controls.Add(this.ListFallbackItems);
+            this.GrpFallbackPool.Name = "GrpFallbackPool";
+            this.GrpFallbackPool.TabStop = false;
             // 
             // ListFallbackItems
             // 
@@ -384,12 +384,12 @@ namespace GrasscutterTools.Forms
             // 
             resources.ApplyResources(this.ColFallbackName, "ColFallbackName");
             // 
-            // GrpYellowPool
+            // GrpUpPool
             // 
-            resources.ApplyResources(this.GrpYellowPool, "GrpYellowPool");
-            this.GrpYellowPool.Controls.Add(this.ListUpItems);
-            this.GrpYellowPool.Name = "GrpYellowPool";
-            this.GrpYellowPool.TabStop = false;
+            resources.ApplyResources(this.GrpUpPool, "GrpUpPool");
+            this.GrpUpPool.Controls.Add(this.ListUpItems);
+            this.GrpUpPool.Name = "GrpUpPool";
+            this.GrpUpPool.TabStop = false;
             // 
             // ListUpItems
             // 
@@ -445,16 +445,16 @@ namespace GrasscutterTools.Forms
             this.BtnParse.UseVisualStyleBackColor = true;
             this.BtnParse.Click += new System.EventHandler(this.BtnParse_Click);
             // 
-            // groupBox1
+            // GrpWeights
             // 
-            resources.ApplyResources(this.groupBox1, "groupBox1");
-            this.groupBox1.Controls.Add(this.LnkWeightHelp);
-            this.groupBox1.Controls.Add(this.TxtWeight4);
-            this.groupBox1.Controls.Add(this.TxtWeight5);
-            this.groupBox1.Controls.Add(this.LblWeight4);
-            this.groupBox1.Controls.Add(this.LblWeight5);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.TabStop = false;
+            resources.ApplyResources(this.GrpWeights, "GrpWeights");
+            this.GrpWeights.Controls.Add(this.LnkWeightHelp);
+            this.GrpWeights.Controls.Add(this.TxtWeight4);
+            this.GrpWeights.Controls.Add(this.TxtWeight5);
+            this.GrpWeights.Controls.Add(this.LblWeight4);
+            this.GrpWeights.Controls.Add(this.LblWeight5);
+            this.GrpWeights.Name = "GrpWeights";
+            this.GrpWeights.TabStop = false;
             // 
             // LnkWeightHelp
             // 
@@ -483,15 +483,15 @@ namespace GrasscutterTools.Forms
             resources.ApplyResources(this.LblWeight5, "LblWeight5");
             this.LblWeight5.Name = "LblWeight5";
             // 
-            // groupBox2
+            // GrpBalance
             // 
-            resources.ApplyResources(this.groupBox2, "groupBox2");
-            this.groupBox2.Controls.Add(this.TxtPoolWeight4);
-            this.groupBox2.Controls.Add(this.TxtPoolWeight5);
-            this.groupBox2.Controls.Add(this.LblPoolWeight4);
-            this.groupBox2.Controls.Add(this.LblPoolWeight5);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.TabStop = false;
+            resources.ApplyResources(this.GrpBalance, "GrpBalance");
+            this.GrpBalance.Controls.Add(this.TxtPoolWeight4);
+            this.GrpBalance.Controls.Add(this.TxtPoolWeight5);
+            this.GrpBalance.Controls.Add(this.LblPoolWeight4);
+            this.GrpBalance.Controls.Add(this.LblPoolWeight5);
+            this.GrpBalance.Name = "GrpBalance";
+            this.GrpBalance.TabStop = false;
             // 
             // TxtPoolWeight4
             // 
@@ -517,11 +517,11 @@ namespace GrasscutterTools.Forms
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.GrpBalance);
+            this.Controls.Add(this.GrpWeights);
             this.Controls.Add(this.GrpJson);
-            this.Controls.Add(this.GrpPurplePool);
-            this.Controls.Add(this.GrpYellowPool);
+            this.Controls.Add(this.GrpFallbackPool);
+            this.Controls.Add(this.GrpUpPool);
             this.Controls.Add(this.GrpBannerValues);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "FormGachaBannerEditor2";
@@ -532,14 +532,14 @@ namespace GrasscutterTools.Forms
             ((System.ComponentModel.ISupportInitialize)(this.NUDGachaType)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.NUDSortId)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.NUDScheduleId)).EndInit();
-            this.GrpPurplePool.ResumeLayout(false);
-            this.GrpYellowPool.ResumeLayout(false);
+            this.GrpFallbackPool.ResumeLayout(false);
+            this.GrpUpPool.ResumeLayout(false);
             this.GrpJson.ResumeLayout(false);
             this.GrpJson.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
+            this.GrpWeights.ResumeLayout(false);
+            this.GrpWeights.PerformLayout();
+            this.GrpBalance.ResumeLayout(false);
+            this.GrpBalance.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -566,8 +566,8 @@ namespace GrasscutterTools.Forms
         private System.Windows.Forms.Label LblCostItem;
         private System.Windows.Forms.Label LblPrefabPath;
         private System.Windows.Forms.RadioButton RbCostItem223;
-        private System.Windows.Forms.GroupBox GrpPurplePool;
-        private System.Windows.Forms.GroupBox GrpYellowPool;
+        private System.Windows.Forms.GroupBox GrpFallbackPool;
+        private System.Windows.Forms.GroupBox GrpUpPool;
         private System.Windows.Forms.GroupBox GrpJson;
         private System.Windows.Forms.TextBox TxtJson;
         private System.Windows.Forms.Button BtnGen;
@@ -584,8 +584,8 @@ namespace GrasscutterTools.Forms
         private System.Windows.Forms.ColumnHeader ColFallbackName;
         private System.Windows.Forms.ColumnHeader ColUpId;
         private System.Windows.Forms.ColumnHeader ColUpName;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox GrpWeights;
+        private System.Windows.Forms.GroupBox GrpBalance;
         private System.Windows.Forms.TextBox TxtWeight4;
         private System.Windows.Forms.TextBox TxtWeight5;
         private System.Windows.Forms.Label LblWeight4;
