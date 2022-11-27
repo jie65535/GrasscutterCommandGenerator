ba4837f1f87df0a601e3736cd0218ac61198a706
筱傑 <jie65535@qq.com>
2022-11-27 16:27:56 +0800

    Update FileVersion to 1.7.4

--- Source/GrasscutterTools/Properties/AssemblyInfo.cs
+++ Source/GrasscutterTools/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.7.4")]
-[assembly: AssemblyFileVersion("1.7.0")]
+[assembly: AssemblyFileVersion("1.7.4")]
