085669e43a018e3c3d29eaef6ba51d57f695c30d
筱傑 <jie65535@qq.com>
2023-10-13 23:08:43 +0800

    Add proxy page translation

--- Source/GrasscutterTools/Pages/PageProxy.Designer.cs
+++ Source/GrasscutterTools/Pages/PageProxy.Designer.cs
@@ -28,11 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PageProxy));
             this.TxtHost = new System.Windows.Forms.TextBox();
             this.LblServerAddress = new System.Windows.Forms.Label();
             this.BtnStartProxy = new System.Windows.Forms.Button();
             this.ChkAutoStart = new System.Windows.Forms.CheckBox();
-            this.LblProxyIntroduction = new System.Windows.Forms.Label();
+            this.LblProxyIntroduction = new System.Windows.Forms.TextBox();
             this.BtnDestroyCert = new System.Windows.Forms.Button();
             this.LnkEavesdrop = new System.Windows.Forms.LinkLabel();
             this.LnkAbout = new System.Windows.Forms.LinkLabel();
@@ -43,124 +44,75 @@
             // 
             // TxtHost
             // 
-            this.TxtHost.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.TxtHost.Location = new System.Drawing.Point(227, 40);
+            resources.ApplyResources(this.TxtHost, "TxtHost");
             this.TxtHost.Name = "TxtHost";
-            this.TxtHost.Size = new System.Drawing.Size(160, 23);
-            this.TxtHost.TabIndex = 4;
-            this.TxtHost.Text = "http://127.0.0.1:443";
             // 
             // LblServerAddress
             // 
-            this.LblServerAddress.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.LblServerAddress.AutoSize = true;
-            this.LblServerAddress.Location = new System.Drawing.Point(84, 43);
+            resources.ApplyResources(this.LblServerAddress, "LblServerAddress");
             this.LblServerAddress.Name = "LblServerAddress";
-            this.LblServerAddress.Size = new System.Drawing.Size(137, 17);
-            this.LblServerAddress.TabIndex = 3;
-            this.LblServerAddress.Text = "目标 http(s) 服务器地址";
             // 
             // BtnStartProxy
             // 
-            this.BtnStartProxy.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.BtnStartProxy.Location = new System.Drawing.Point(393, 40);
+            resources.ApplyResources(this.BtnStartProxy, "BtnStartProxy");
             this.BtnStartProxy.Name = "BtnStartProxy";
-            this.BtnStartProxy.Size = new System.Drawing.Size(120, 25);
-            this.BtnStartProxy.TabIndex = 5;
-            this.BtnStartProxy.Text = "启动代理";
             this.BtnStartProxy.UseVisualStyleBackColor = true;
             this.BtnStartProxy.Click += new System.EventHandler(this.BtnStartProxy_Click);
             // 
             // ChkAutoStart
             // 
-            this.ChkAutoStart.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.ChkAutoStart.AutoSize = true;
-            this.ChkAutoStart.Location = new System.Drawing.Point(519, 42);
+            resources.ApplyResources(this.ChkAutoStart, "ChkAutoStart");
             this.ChkAutoStart.Name = "ChkAutoStart";
-            this.ChkAutoStart.Size = new System.Drawing.Size(75, 21);
-            this.ChkAutoStart.TabIndex = 6;
-            this.ChkAutoStart.Text = "自动开启";
             this.ChkAutoStart.UseVisualStyleBackColor = true;
             this.ChkAutoStart.CheckedChanged += new System.EventHandler(this.ChkAutoStart_CheckedChanged);
             // 
             // LblProxyIntroduction
             // 
-            this.LblProxyIntroduction.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.LblProxyIntroduction.Location = new System.Drawing.Point(48, 72);
+            resources.ApplyResources(this.LblProxyIntroduction, "LblProxyIntroduction");
             this.LblProxyIntroduction.Name = "LblProxyIntroduction";
-            this.LblProxyIntroduction.Size = new System.Drawing.Size(550, 120);
-            this.LblProxyIntroduction.TabIndex = 7;
-            this.LblProxyIntroduction.Text = "    启动代理需要信任本应用的临时根证书，该证书仅用于代理动漫游戏相关请求，有效期1月，你可以随时点击右下角卸载证书。\r\n    代理功能代码来自开源项目 Ea" +
-    "vesdrop，遵循MIT开源协议，经过魔改以匹配应用需求。\r\n    本代理通过大量过滤规则来规避非动漫游戏的请求经过应用，减小影响，欢迎体验！\r\n    程" +
-    "序退出时会自动关闭代理，放心使用 :)";
+            this.LblProxyIntroduction.ReadOnly = true;
             // 
             // BtnDestroyCert
             // 
-            this.BtnDestroyCert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.BtnDestroyCert.Location = new System.Drawing.Point(529, 207);
+            resources.ApplyResources(this.BtnDestroyCert, "BtnDestroyCert");
             this.BtnDestroyCert.Name = "BtnDestroyCert";
-            this.BtnDestroyCert.Size = new System.Drawing.Size(100, 25);
-            this.BtnDestroyCert.TabIndex = 10;
-            this.BtnDestroyCert.Text = "卸载证书";
             this.BtnDestroyCert.UseVisualStyleBackColor = true;
             this.BtnDestroyCert.Click += new System.EventHandler(this.BtnDestroyCert_Click);
             // 
             // LnkEavesdrop
             // 
-            this.LnkEavesdrop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.LnkEavesdrop.AutoSize = true;
-            this.LnkEavesdrop.Location = new System.Drawing.Point(15, 211);
+            resources.ApplyResources(this.LnkEavesdrop, "LnkEavesdrop");
             this.LnkEavesdrop.Name = "LnkEavesdrop";
-            this.LnkEavesdrop.Size = new System.Drawing.Size(70, 17);
-            this.LnkEavesdrop.TabIndex = 8;
             this.LnkEavesdrop.TabStop = true;
-            this.LnkEavesdrop.Text = "Eavesdrop";
             this.LnkEavesdrop.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LnkEavesdrop_LinkClicked);
             // 
             // LnkAbout
             // 
-            this.LnkAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.LnkAbout.AutoSize = true;
-            this.LnkAbout.Location = new System.Drawing.Point(482, 211);
+            resources.ApplyResources(this.LnkAbout, "LnkAbout");
             this.LnkAbout.Name = "LnkAbout";
-            this.LnkAbout.Size = new System.Drawing.Size(32, 17);
-            this.LnkAbout.TabIndex = 9;
             this.LnkAbout.TabStop = true;
-            this.LnkAbout.Text = "关于";
             this.LnkAbout.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LnkAbout_LinkClicked);
             // 
             // LblGcJarPath
             // 
-            this.LblGcJarPath.AutoSize = true;
-            this.LblGcJarPath.Location = new System.Drawing.Point(102, 14);
+            resources.ApplyResources(this.LblGcJarPath, "LblGcJarPath");
             this.LblGcJarPath.Name = "LblGcJarPath";
-            this.LblGcJarPath.Size = new System.Drawing.Size(119, 17);
-            this.LblGcJarPath.TabIndex = 0;
-            this.LblGcJarPath.Text = "GrasscutterJar 路径";
-            this.LblGcJarPath.Visible = false;
             // 
             // TxtGcJarPath
             // 
-            this.TxtGcJarPath.Location = new System.Drawing.Point(227, 11);
+            resources.ApplyResources(this.TxtGcJarPath, "TxtGcJarPath");
             this.TxtGcJarPath.Name = "TxtGcJarPath";
-            this.TxtGcJarPath.Size = new System.Drawing.Size(160, 23);
-            this.TxtGcJarPath.TabIndex = 1;
-            this.TxtGcJarPath.Visible = false;
             // 
             // BtnStartGc
             // 
-            this.BtnStartGc.Location = new System.Drawing.Point(393, 11);
+            resources.ApplyResources(this.BtnStartGc, "BtnStartGc");
             this.BtnStartGc.Name = "BtnStartGc";
-            this.BtnStartGc.Size = new System.Drawing.Size(120, 25);
-            this.BtnStartGc.TabIndex = 2;
-            this.BtnStartGc.Text = "启动服务器";
             this.BtnStartGc.UseVisualStyleBackColor = true;
-            this.BtnStartGc.Visible = false;
             this.BtnStartGc.Click += new System.EventHandler(this.BtnStartGc_Click);
             // 
             // PageProxy
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.BtnStartGc);
             this.Controls.Add(this.TxtGcJarPath);
@@ -185,7 +137,7 @@
         private System.Windows.Forms.Label LblServerAddress;
         private System.Windows.Forms.Button BtnStartProxy;
         private System.Windows.Forms.CheckBox ChkAutoStart;
-        private System.Windows.Forms.Label LblProxyIntroduction;
+        private System.Windows.Forms.TextBox LblProxyIntroduction;
         private System.Windows.Forms.Button BtnDestroyCert;
         private System.Windows.Forms.LinkLabel LnkEavesdrop;
         private System.Windows.Forms.LinkLabel LnkAbout;
